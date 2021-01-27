9c7617ac5e0fe22d81047230908a964c8504af99
Kevin McAteer <kmcateer926@gmail.com>
2021-01-27 16:14:07 -0500

    starting initial setup with server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = require("express");
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/api/config", (req, res) => {
+  res.json({
+    success: true,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on http://localhost"${PORT}`);
+});
