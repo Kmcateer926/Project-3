5c090e18178264e7cc64d80710660b7681f6d2e3
Kevin McAteer <kmcateer926@gmail.com>
2021-01-27 17:06:43 -0500

    added proxy

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 const express = require("express");
-const app = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
 
-const PORT = process.env.PORT || 3000;
+const app = express();
+
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static("client/build"));
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/project-3",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  }
+);
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+  console.log("Mongoose successfully connected!");
+});
+
+connection.on("error", (err) => {
+  console.log("Mongoose connection error: ", err);
+});
 
 app.get("/api/config", (req, res) => {
   res.json({
@@ -12,6 +37,10 @@ app.get("/api/config", (req, res) => {
   });
 });
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
+
 app.listen(PORT, () => {
-  console.log(`server is running on http://localhost"${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
