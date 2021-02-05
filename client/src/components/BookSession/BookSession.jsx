52456c2893ceb01c926e78c77b528a9fbdee6c8d
Kevin McAteer <kmcateer926@gmail.com>
2021-02-04 22:05:53 -0500

    trying to set state

--- client/src/components/BookSession/BookSession.jsx
+++ client/src/components/BookSession/BookSession.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./Directory.css";
 import teacherDirectory from "./teacherDirectory.json";
 import CalendarApp from "../../components/Calendar/Calendar";
-
+import { Link } from "react-router-dom";
 
 const BookSessionForm = ({ handleFormSubmit }) => {
   const [tutor, setTutor] = useState("");
@@ -23,102 +23,102 @@ const BookSessionForm = ({ handleFormSubmit }) => {
   const [teachers, setTeachers] = useState({});
 
   useEffect(() => {
-      setTeachers(teacherDirectory);
+    setTeachers(teacherDirectory);
   }, [teachers]);
 
   return (
-      <>
-          <div className="spacer" style={{ height: "50px" }}></div>
-          <div>
-              <h1
-                  style={{
-                      fontFamily: "Special Elite, cursive",
-                      fontSize: "50px",
-                      textAlign: "center",
-                  }}
-              >
-                  Tutor Directory
-              </h1>
-              <div className="spacer" style={{ height: "50px" }}></div>
+    <>
+      <div className="spacer" style={{ height: "50px" }}></div>
+      <div>
+        <h1
+          style={{
+            fontFamily: "Special Elite, cursive",
+            fontSize: "50px",
+            textAlign: "center",
+          }}
+        >
+          Tutor Directory
+        </h1>
+        <div className="spacer" style={{ height: "50px" }}></div>
+      </div>
+      {teacherDirectory.map((teachers) => (
+        <div
+          className="card "
+          style={{
+            margin: "10px",
+            border: "1px solid",
+            padding: "5px",
+            width: "30%",
+            display: "inline-block",
+            flexDirection: "row",
+            boxShadow: "5px 2px 2px grey",
+            borderRadius: "15px",
+          }}
+        >
+          <div className="card-image">
+            <figure className="image is-128x128">
+              <img
+                src={teachers.image}
+                alt={teachers.name}
+                style={{ borderRadius: "15px" }}
+              />
+            </figure>
           </div>
-          {teacherDirectory.map((teachers) => (
-              <div
-                  className="card "
-                  style={{
-                      margin: "10px",
-                      border: "1px solid",
-                      padding: "5px",
-                      width: "30%",
-                      display: "inline-block",
-                      flexDirection: "row",
-                      boxShadow: "5px 2px 2px grey",
-                      borderRadius: "15px",
-                  }}
-              >
-                  <div className="card-image">
-                      <figure className="image is-128x128">
-                          <img
-                              src={teachers.image}
-                              alt={teachers.name}
-                              style={{ borderRadius: "15px" }}
-                          />
-                      </figure>
-                  </div>
 
-
-        <div className="card">
-       
-          <div className="card-content">
-               <form
-            className="column"
-            onSubmit={(e) => {
-              handleFormSubmit(e, {
-                tutor,
-                sessionLength,
-                date,
-                time,
-                // userCreated,
-                // subjects,
-                // id,
-              });
-            }}
-          ></form>
-            <div
-              className="content"
-              style={{ fontFamily: "Special Elite, cursive" }}
-            >
-              <strong>Name: </strong>
-              {teachers.name}
-              <br />
-              <strong>Education: </strong>
-              {teachers.education}
-              <br />
-              <strong>Experience: </strong>
-              {teachers.experience}
-              <br />
-              <strong>Subject(s): </strong>
-              {teachers.subjects}
-              <br />
-              <br />
-              <button
-                className="button is-small is-fullwidth is-info"
-                style={{
-                  borderRadius: "10px",
-                  fontSize: "25px",
-                  fontWeight: "bold",
-                  fontFamily: "Special Elite, cursive",
+          <div className="card">
+            <div className="card-content">
+              <form
+                className="column"
+                onSubmit={(e) => {
+                  handleFormSubmit(e, {
+                    tutor,
+                    sessionLength,
+                    date,
+                    time,
+                    // userCreated,
+                    // subjects,
+                    // id,
+                  });
                 }}
+              ></form>
+              <div
+                className="content"
+                style={{ fontFamily: "Special Elite, cursive" }}
               >
-                {/* TODO: add onclick to prompt teacher schedule */}
-                Book Session
-              </button>
-              <CalendarApp></CalendarApp>
+                <strong>Name: </strong>
+                {teachers.name}
+                <br />
+                <strong>Education: </strong>
+                {teachers.education}
+                <br />
+                <strong>Experience: </strong>
+                {teachers.experience}
+                <br />
+                <strong>Subject(s): </strong>
+                {teachers.subjects}
+                <br />
+                <br />
+                <button
+                  className="button is-small is-fullwidth is-info"
+                  style={{
+                    borderRadius: "10px",
+                    fontSize: "25px",
+                    fontWeight: "bold",
+                    fontFamily: "Special Elite, cursive",
+                  }} onClick={handleFormSubmit}
+                >
+                 
+                  <Link to="/landing" style={styles.link}>Book Session</Link>
+                  
+                </button>
+
+                <CalendarApp></CalendarApp>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    ))}
-  </>
-);
+      ))}
+    </>
+  );
 };
 export default BookSessionForm;
