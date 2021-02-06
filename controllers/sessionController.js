0e91f2de9000e5ab5b5694ab5b53b53ec9ae23ce
Geoff Nix <FullStackGeoff@gmail.com>
2021-02-06 12:43:31 -0500

    delete function

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -4,44 +4,56 @@ const router = express.Router();
 const Session = require("../models/session");
 
 router.get("/", (req, res) => {
-  Session.find({
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((sessions) => {
-      res.json(sessions);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).end();
-    });
+	Session.find({})
+		.then((sessions) => {
+      console.log(sessions);
+			res.json(sessions);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).end();
+		});
+});
+
+router.get("/:id", (req, res) => {
+	Session.find({
+		where: {
+			id: req.params.id,
+		},
+	})
+		.then((sessions) => {
+			res.json(sessions);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).end();
+		});
 });
 
 router.post("/", (req, res) => {
-  console.log(req.body);
-  Session.create(req.body)
-    .then((newSession) => {
-      console.log(newSession);
-      res.json(newSession);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+	console.log(req.body);
+	Session.create(req.body)
+		.then((newSession) => {
+			console.log(newSession);
+			res.json(newSession);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 });
 
 router.put("/:id", (req, res) => {
-  Session.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(
-    (updatedObject) => {
-      res.json(updatedObject);
-    }
-  );
+	Session.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(
+		(updatedObject) => {
+			res.json(updatedObject);
+		}
+	);
 });
 
 router.delete("/:id", (req, res) => {
-  Session.findByIdAndDelete(req.params.id).then((result) => {
-    res.json(result);
-  });
+	Session.findByIdAndDelete(req.params.id).then((result) => {
+		res.json(result);
+	});
 });
 
 module.exports = router;
