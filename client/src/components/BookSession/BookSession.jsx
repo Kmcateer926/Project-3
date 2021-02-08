ae9fba77fda108369d2b39e311f78518c52262ea
Geoff Nix <FullStackGeoff@gmail.com>
2021-02-08 17:18:56 -0500

    added new landing table

--- client/src/components/BookSession/BookSession.jsx
+++ client/src/components/BookSession/BookSession.jsx
@@ -2,191 +2,30 @@ import React, { useState, useEffect } from "react";
 import "./Directory.css";
 import teacherDirectory from "./teacherDirectory.json";
 import CalendarApp from "../../components/Calendar/Calendar";
-import { Link, useHistory } from "react-router-dom";
+import { Link, useHistory, useParams } from "react-router-dom";
 import axios from "axios";
-const BookSessionForm = ({ handleFormSubmit }) => {
-	const [tutor, setTutor] = useState("");
-	const [sessionLength, setSessionLength] = useState("");
 
-	const [date, setDate] = useState("");
-	const [time, setTime] = useState("");
-	const history = useHistory();
-	const styles = {
-		font: {
-			fontFamily: "Special Elite, cursive",
-		},
-		heading: {
-			paddingTop: 40,
-		},
-	};
 
-	const [teachers, setTeachers] = useState({});
+const BookSessionForm = ({ buttonText, handleFormSubmit }) => {
 
-	useEffect(() => {
-		setTeachers(teacherDirectory);
-	}, [teachers]);
+  const [tutor, setTutor] = useState("");
+  const [sessionLength, setSessionLength] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
 
-	function handleFormSubmit(event) {
-		event.preventDefault();
-		const sessionData = {
-			tutor,
-			sessionLength,
-			date,
-			time,
-		};
+  const { id } = useParams();
 
-		axios
-			.post("/api/sessions/landing", sessionData)
-			.then(() => {
-				history.push("/landing");
-				alert("Successfully Logged in user");
-			})
-			.catch((err) => console.log(err));
-	}
+  const history = useHistory();
 
-	return (
-		<>
-			<div className="spacer" style={{ height: "50px" }}></div>
-			<div>
-				<h1
-					style={{
-						fontFamily: "Special Elite, cursive",
-						fontSize: "50px",
-						textAlign: "center",
-					}}
-				>
-					Tutor Directory
-				</h1>
-				<div className="spacer" style={{ height: "50px" }}></div>
-			</div>
-			{teacherDirectory.map((teachers, index) => (
-				<div
-					key={teachers.id}
-					className="card "
-					style={{
-						margin: "10px",
-						border: "1px solid",
-						padding: "5px",
-						width: "30%",
-						display: "inline-block",
-						flexDirection: "row",
-						boxShadow: "5px 2px 2px grey",
-						borderRadius: "15px",
-					}}
-				>
-					<div className="card-image">
-						<figure className="image is-128x128">
-							<img
-								src={teachers.image}
-								alt={teachers.name}
-								style={{ borderRadius: "15px" }}
-							/>
-						</figure>
-					</div>
+  const styles = {
+    font: {
+      fontFamily: "Special Elite, cursive",
+    },
+    heading: {
+      paddingTop: 40,
+    },
+  };
 
-<<<<<<< HEAD
-					<div className="card">
-						<div className="card-content">
-							<form
-								className="column"
-								onSubmit={(e) => {
-									handleFormSubmit(e, {
-										tutor,
-										sessionLength,
-										date,
-										time,
-										// userCreated,
-										// subjects,
-										// id,
-									});
-								}}
-							></form>
-							<div
-								className="content"
-								style={{ fontFamily: "Special Elite, cursive" }}
-							>
-								<strong>Name: </strong>
-								{teachers.name}
-								<br />
-								<strong>Education: </strong>
-								{teachers.education}
-								<br />
-								<strong>Experience: </strong>
-								{teachers.experience}
-								<br />
-								<strong>Subject(s): </strong>
-								{teachers.subjects}
-								<br />
-								<p>
-									<strong>
-										Session Length:{" "}
-										<input
-											className="input is-danger"
-											type="integer"
-											placeholder="Session Length"
-											id="sessionLength"
-											value={sessionLength}
-											onChange={(e) => {
-												setSessionLength(
-													e.target.value
-												);
-											}}
-										/>
-									</strong>
-								</p>
-								<p>
-									<strong>
-										Date:{" "}
-										<input
-											className="input is-danger"
-											type="date"
-											placeholder="Session Length"
-											id="date"
-											value={date}
-											onChange={(e) => {
-												setDate(e.target.value);
-											}}
-										/>
-									</strong>
-								</p>
-								<p>
-									<strong>
-										Time:{" "}
-										<input
-											className="input is-danger"
-											type="integer"
-											placeholder="Session Length"
-											id="time"
-											value={time}
-											onChange={(e) => {
-												setTime(e.target.value);
-											}}
-										/>
-									</strong>
-								</p>
-								<br />
-								<button
-									className="button is-small is-fullwidth is-info"
-									style={{
-										borderRadius: "10px",
-										fontSize: "25px",
-										fontWeight: "bold",
-										fontFamily: "Special Elite, cursive",
-									}}
-									onClick={handleFormSubmit}
-								>
-									<Link to="/landing" style={styles.link}>
-										Book Session
-									</Link>
-								</button>
-							</div>
-						</div>
-					</div>
-				</div>
-			))}
-		</>
-	);
-=======
   useEffect(()=>{
     console.log(id);
     axios.post(`/api/sessions/${id}`)
@@ -209,30 +48,7 @@ const BookSessionForm = ({ handleFormSubmit }) => {
     });
     }, [id])
 
-  // const [teachers, setTeachers] = useState({});
-
-  // useEffect(() => {
-  //   console.log(id)
-  //   setTutor(teacherDirectory);
-  // }, [id]);
-
-  // function handleFormSubmit(event) {
-  //   event.preventDefault();
-  //   const sessionData = {
-  //     tutor,
-  //     sessionLength,
-  //     date,
-  //     time,
-  //   };
-
-  //   axios
-  //     .post("/api/sessions/landing", sessionData)
-  //     .then(() => {
-  //       history.push("/landing");
-  //       alert("Successfully Logged in user");
-  //     })
-  //     .catch((err) => console.log(err));
-  // }
+  
 
   return (
     <>
@@ -262,7 +78,7 @@ const BookSessionForm = ({ handleFormSubmit }) => {
             boxShadow: "5px 2px 2px grey",
             borderRadius: "15px",
           }}
-        >e
+        >
           <div className="card-image">
             <figure className="image is-128x128">
               <img
@@ -294,33 +110,18 @@ const BookSessionForm = ({ handleFormSubmit }) => {
                 style={{ fontFamily: "Special Elite, cursive" }}
               >
                 <strong>Name: </strong>
-                {/* {teachers.name} */}
-                <input
-                      className="input is-danger"
-                      type="text"
-                      id="tutor"
-                      value={tutor}
-                      onChange={(e) => {
-                        setTutor(e.target.value);
-                      }}
-                    />
+                {teachers.name}
                 <br />
-               
-                {/* <p>
-                  <strong>
-                    Session Length:{" "}
-                    <input
-                      className="input is-danger"
-                      type="integer"
-                      placeholder="Session Length"
-                      id="email"
-                      value={sessionLength}
-                      onChange={(e) => {
-                        setSessionLength(e.target.value);
-                      }}
-                    />
-                  </strong>
-                </p> */}
+                <strong>Education: </strong>
+                {teachers.education}
+                <br />
+                <strong>Experience: </strong>
+                {teachers.experience}
+                <br />
+                <strong>Subject(s): </strong>
+                {teachers.subjects}
+                <br /> <br /> <br />
+                
               </div>
               <div className="booking-deets">
                 <p>
@@ -374,16 +175,7 @@ const BookSessionForm = ({ handleFormSubmit }) => {
                         </div>
                       </div>
                     </div>
-                    {/* <input
-                      className="input is-danger"
-                      type="integer"
-                      placeholder="Session Length"
-                      id="email"
-                      value={time}
-                      onChange={(e) => {
-                        setTime(e.target.value);
-                      }}
-                    /> */}
+                    
                   </strong>
                 </p>
 
@@ -408,6 +200,5 @@ const BookSessionForm = ({ handleFormSubmit }) => {
       ))}
     </>
   );
->>>>>>> 3a9208b1c518c2af14ac7cdffcb14439b2a70d17
 };
 export default BookSessionForm;
