3b2f02d7f9e4914fe5707c6a48adb7aa3ee48502
Geoff Nix <FullStackGeoff@gmail.com>
2021-02-12 00:42:35 -0500

    styled directory page

--- client/src/components/TutorCard/TutorCard.jsx
+++ client/src/components/TutorCard/TutorCard.jsx
@@ -2,82 +2,94 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link, useHistory } from "react-router-dom";
 import axios from "axios";
-
+import board from "../ChalkBG/chalkBoard.jpg";
 import SessionForm from "../../components/SessionForm/SessionForm";
 
-const TutorCard = ({ name, imageURL, education, subjects, _id, experience, email }) => {
-  const history = useHistory();
+const TutorCard = ({
+	name,
+	imageURL,
+	education,
+	subjects,
+	_id,
+	experience,
+	email,
+}) => {
+	const history = useHistory();
 
-  const handleFormSubmit = (event, formData) => {
-    event.preventDefault();
-    axios.post("/api/sessions", formData).then((response) => {
-      console.log(response);
-      history.push("/landing");
-    });
-  };
+	const handleFormSubmit = (event, formData) => {
+		event.preventDefault();
+		axios.post("/api/sessions", formData).then((response) => {
+			console.log(response);
+			history.push("/landing");
+		});
+	};
 
-  return (
-    <div className="container">
-      <div className="columns">
-        <div className="column is-3">
-          <div className="card" style={{ display: "inline-block" }}>
-            <header class="card-header">
-              <p class="card-header-title">{name}</p>
-              <img src={imageURL} alt={name} />
-            </header>
-            <span className="card-title"></span>
-            <div className="card-content">
-              <div className="content">
-                <p>
-                  <strong>Education: {education}</strong>
-                </p>
-                <p>
-                  <strong>Subjects(s): {subjects}</strong>
-                </p>
-                <p>
-                  <strong>Experience(Yrs): {experience}</strong>
-                </p>
-              </div>
-              <SessionForm
-                handleFormSubmit={handleFormSubmit}
-                buttonText="Book Session"
-              ></SessionForm>
-              {/* <footer class="card-footer">
-                <Link to="/landing" className="card-footer-item">
-                  Book Session
-                </Link>
-              </footer> */}
+	return (
+		<>
+			{/* <div
+				className="columns"
+				style={{
+					display: "flex",
+					flexWrap: "wrap",
+					// justifyContent: "center",
+					alignItems: "center",
+				}}
+			> */}
+			<div
+				className=" card column is-3"
+				style={{
+					margin: "20px",
+					border: "solid 2px white",
+					float: "left",
+					borderRadius: "15px",
+					padding: "10px",
+					flexDirection: "row",
+					// boxShadow: "5px 5px 5px grey",
+					display: "inline-block",
+          fontFamily: "Special Elite, cursive",
+          backgroundImage:`url(${board})`,
+          color: "white",
+          
+				}}
+			>
+				<h1 class="title" style={{color:"white"}}>{name}</h1>
+				<p>
+					<img src={imageURL} alt={name} />
+				</p>
 
-              <div className="card-action">
-                <Link to={`/tutors/${_id}`}></Link>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* <div className="row">
-					<div className="col s12 m7">
-						<div className="card">
-							<div>
-								<SessionForm
-									//   handleFormSubmit={handleFormSubmit}
-									buttonText="Create New Session"
-								/>
-							</div>
-						</div>
-					</div>
-				</div> */}
-      </div>
-    </div>
-  );
+				<div style={{ fontSize: "18px"}}>
+					<p style={{color:"white"}}>
+						<strong style={{color:"white"}}>Education: {education}</strong>
+					</p>
+					<p>
+						<strong style={{color:"white"}}>Subjects(s): {subjects}</strong>
+					</p>
+					<p>
+						<strong style={{color:"white"}}>Experience(Yrs): {experience}</strong>
+					</p>
+				</div>
+				<hr />
+				<SessionForm
+					handleFormSubmit={handleFormSubmit}
+					buttonText="Book Session"
+				></SessionForm>
+			</div>
+
+			<div className="card-action">
+				<Link to={`/tutors/${_id}`}></Link>
+			</div>
+			{/* </div> */}
+		</>
+	);
 };
 
 TutorCard.propTypes = {
-  _id: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  education: PropTypes.string.isRequired,
-  subjects: PropTypes.string.isRequired,
-  experience: PropTypes.number.isRequired,
-  email: PropTypes.string.isRequired,
+	_id: PropTypes.string.isRequired,
+	name: PropTypes.string.isRequired,
+	education: PropTypes.string.isRequired,
+	subjects: PropTypes.string.isRequired,
+	experience: PropTypes.number.isRequired,
+	email: PropTypes.string.isRequired,
 };
 
 export default TutorCard;
