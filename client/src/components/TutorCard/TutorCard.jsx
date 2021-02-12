48729f20b11731d64048e63653941dde6198a97d
Estefania <nialupo87@gmail.com>
2021-02-11 20:37:37 -0500

    directory showing on landing, eding working

--- client/src/components/TutorCard/TutorCard.jsx
+++ client/src/components/TutorCard/TutorCard.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import axios from "axios";
 
 import SessionForm from "../../components/SessionForm/SessionForm";
+
 const TutorCard = ({ name, education, subjects, _id, experience, email }) => {
+  const history = useHistory();
+
+  const handleFormSubmit = (event, formData) => {
+    event.preventDefault();
+    axios.post("/api/sessions", formData).then((response) => {
+      console.log(response);
+      history.push("/landing");
+    });
+  };
+
   return (
     <div className="container">
       <div className="columns">
@@ -24,12 +36,15 @@ const TutorCard = ({ name, education, subjects, _id, experience, email }) => {
                   <strong>Experience(Yrs): {experience}</strong>
                 </p>
               </div>
-              <SessionForm></SessionForm>
-              <footer class="card-footer">
-                <a href="/" className="card-footer-item">
+              <SessionForm
+                handleFormSubmit={handleFormSubmit}
+                buttonText="Book Session"
+              ></SessionForm>
+              {/* <footer class="card-footer">
+                <Link to="/landing" className="card-footer-item">
                   Book Session
-                </a>
-              </footer>
+                </Link>
+              </footer> */}
 
               <div className="card-action">
                 <Link to={`/tutors/${_id}`}></Link>
--- client/src/containers/EditSessionTest/EditSessionTest.jsx
+++ client/src/containers/EditSessionTest/EditSessionTest.jsx
@@ -6,7 +6,7 @@ import EditBookSession from "../../components/EditBookSession/EditBookSession";
 // import ChalkBG from "../../components/ChalkBG/ChalkBG";
 
 const EditSessionTest = () => {
-  // const history = useHistory();
+  const history = useHistory();
 
   const handleFormSubmit = (e, sessionData, id) => {
     console.log(sessionData);
@@ -16,7 +16,7 @@ const EditSessionTest = () => {
       .put(`/api/sessions/${id}`, sessionData)
       .then((response) => {
         console.log(response.data);
-        // history.push("/landing");
+        history.push("/landing");
       })
       .catch((err) => {
         console.log(err);
