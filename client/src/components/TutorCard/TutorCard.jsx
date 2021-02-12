e3e859f6d13c2cafa69bc283f2a6fa2de52270d5
Kevin McAteer <kmcateer926@gmail.com>
2021-02-11 19:35:07 -0500

    added input fields

--- client/src/components/TutorCard/TutorCard.jsx
+++ client/src/components/TutorCard/TutorCard.jsx
@@ -4,44 +4,40 @@ import { Link } from "react-router-dom";
 
 import SessionForm from "../../components/SessionForm/SessionForm";
 const TutorCard = ({ name, education, subjects, _id, experience, email }) => {
-	return (
-		<div className="container">
-			<div className="columns">
-				<div className="column is-3">
-					<div className="card" style={{display:"inline-block"}}>
-						<header class="card-header">
-							<p class="card-header-title">{name}</p>
-						</header>
-						<div className="card-content">
-							<div className="content">
-								<p>
-									<strong>Education: {education}</strong>
-								</p>
-								<p>
-									<strong>Subjects(s): {subjects}</strong>
-								</p>
-								<p>
-									<strong>
-										Experience(Yrs): {experience}
-									</strong>
-								</p>
-							</div>
-<SessionForm></SessionForm>
-							<footer class="card-footer">
-								<a href="/" className="card-footer-item">
-									Book Session
-								</a>
-							</footer>
+  return (
+    <div className="container">
+      <div className="columns">
+        <div className="column is-3">
+          <div className="card" style={{ display: "inline-block" }}>
+            <header class="card-header">
+              <p class="card-header-title">{name}</p>
+            </header>
+            <div className="card-content">
+              <div className="content">
+                <p>
+                  <strong>Education: {education}</strong>
+                </p>
+                <p>
+                  <strong>Subjects(s): {subjects}</strong>
+                </p>
+                <p>
+                  <strong>Experience(Yrs): {experience}</strong>
+                </p>
+              </div>
+              <SessionForm></SessionForm>
+              <footer class="card-footer">
+                <a href="/" className="card-footer-item">
+                  Book Session
+                </a>
+              </footer>
 
-							
-							
-							<div className="card-action">
-								<Link to={`/tutors/${_id}`}></Link>
-							</div>
-						</div>
-					</div>
-				</div>
-				{/* <div className="row">
+              <div className="card-action">
+                <Link to={`/tutors/${_id}`}></Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <div className="row">
 					<div className="col s12 m7">
 						<div className="card">
 							<div>
@@ -53,18 +49,18 @@ const TutorCard = ({ name, education, subjects, _id, experience, email }) => {
 						</div>
 					</div>
 				</div> */}
-			</div>
-		</div>
-	);
+      </div>
+    </div>
+  );
 };
 
 TutorCard.propTypes = {
-	_id: PropTypes.string.isRequired,
-	name: PropTypes.string.isRequired,
-	education: PropTypes.string.isRequired,
-	subjects: PropTypes.string.isRequired,
-	experience: PropTypes.number.isRequired,
-	email: PropTypes.string.isRequired,
+  _id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  education: PropTypes.string.isRequired,
+  subjects: PropTypes.string.isRequired,
+  experience: PropTypes.number.isRequired,
+  email: PropTypes.string.isRequired,
 };
 
 export default TutorCard;
