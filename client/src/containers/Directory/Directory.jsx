61d43de9f1bc4f7dc367da245d81adaa435f1336
Geoff Nix <FullStackGeoff@gmail.com>
2021-02-13 11:08:59 -0500

    cleaned up code

--- client/src/containers/Directory/Directory.jsx
+++ client/src/containers/Directory/Directory.jsx
@@ -1,13 +1,9 @@
 // import React, { useContext } from "react";
-// import { useHistory } from "react-router-dom";
+
 import board from "../../components/ChalkBG/chalkBoard.jpg";
 import React, { useEffect, useState, useHistory } from "react";
 import axios from "axios";
-// import BookSession from "../../components/BookSession/BookSession";
-// import ChalkBG from "../../components/ChalkBG/ChalkBG";
 
-// import React, { useEffect, useState } from "react";
-// import axios from "axios";
 import TutorCard from "../../components/TutorCard/TutorCard";
 
 const Tutors = () => {
@@ -67,59 +63,4 @@ const Tutors = () => {
 
 export default Tutors;
 
-// const NewSession = () => {
 
-//   const history = useHistory()
-
-//   const handleFormSubmit = (e, sessionData) => {
-//     e.preventDefault();
-//     axios
-//       .post("/api/sessions", sessionData)
-//       .then((response) => {
-//         console.log(response.data);
-//         history.push("/landing");
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-//   };
-
-//   // const [sessions, setSessions] = useState([]);
-
-//   // useEffect(()=>{
-//   //   getSessions()
-//   // }, []);
-
-//   // const getSessions = () => {
-//   //   axios.get("/api/sessions")
-//   //   .then((response) => {
-//   //     console.log(response.data);
-//   //     setSessions(response.data)
-//   //   }).catch((err)=> {
-//   //     console.log(err);
-//   //   })
-//   // }
-
-//   return (
-//     <ChalkBG>
-//     <div className="container">
-//       <div className="row">
-//         <div className="col">
-//           {/* <h1 className="center-align">Add a New Parent</h1> */}
-//         </div>
-//       </div>
-//       <div className="row">
-//         <BookSession
-//           handleFormSubmit={handleFormSubmit}
-//           buttonText="Create New Session"
-//         />
-//         {/* {sessions.map((session)=>(
-//  <BookSession {...sessions} getSessions={getSessions}/>
-//         ))} */}
-//       </div>
-//     </div>
-//     </ChalkBG>
-//   );
-// };
-
-// export default NewSession;
