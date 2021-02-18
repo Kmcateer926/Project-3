bbc0cd45ae31840ec18ee1aa0c54713a9d93f670
Kevin McAteer <kmcateer926@gmail.com>
2021-02-18 16:09:47 -0500

    polishing commented code

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,37 +1,35 @@
-import React from 'react';
+import React from "react";
 
 const style = {
-    textAlign: "center",
-    padding: "20px",
-    position: "fixed",
-    left: "0",
-    bottom: "0",
-    height: "60px",
-    width: "100%",
-    background: "#2674DB",
-    opacity:"0.8",
-    color: "white",
-    // borderTop: "8px solid  #94b8b8",
-    flexShrink: "0",
-    fontFamily: "Special Elite, cursive",
-  };
-  const phantom = {
-    display: "block",
-    padding: "20px",
-    height: "60px",
-    width: "100%",
-   
-  };
+  textAlign: "center",
+  padding: "20px",
+  position: "fixed",
+  left: "0",
+  bottom: "0",
+  height: "60px",
+  width: "100%",
+  background: "#2674DB",
+  opacity: "0.8",
+  color: "white",
+  flexShrink: "0",
+  fontFamily: "Special Elite, cursive",
+};
+const phantom = {
+  display: "block",
+  padding: "20px",
+  height: "60px",
+  width: "100%",
+};
 
 const Footer = () => {
-    return (
-        <div>
-        <div style={phantom} />
-        <div style={style}>
-          <div>&copy; Copyright 2021</div>
-        </div>
+  return (
+    <div>
+      <div style={phantom} />
+      <div style={style}>
+        <div>&copy; Copyright 2021</div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Footer;
